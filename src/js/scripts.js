9603beea34eeb4d1da97b0e6271b1afbc0ff6927
unknown <rizal.arfiyan.23@gmail.com>
2021-05-22 08:38:29 +0700

    achivement section

--- src/js/scripts.js
+++ src/js/scripts.js
@@ -13,4 +13,23 @@ function loadScript(url, callback) {
   document.getElementsByTagName('head')[0].appendChild(script)
 }
 
+function resizeGridItem(item){
+  grid = document.getElementsByClassName("achivement-sec")[0];
+  rowHeight = parseInt(window.getComputedStyle(grid).getPropertyValue('grid-auto-rows'));
+  rowGap = parseInt(window.getComputedStyle(grid).getPropertyValue('grid-row-gap'));
+  image = item.querySelector('.image') !== null ? item.querySelector('.image').getBoundingClientRect().height : 0;
+  rowSpan = Math.ceil((item.querySelector('.content').getBoundingClientRect().height + rowGap + image) / (rowHeight + rowGap - 2));
+  item.style.gridRowEnd = `span ${rowSpan}`;
+}
+
+function resizeAllGridItems(){
+  allItems = document.querySelectorAll(".achivement-sec .card");
+  for(x = 0; x < allItems.length; x++){
+    resizeGridItem(allItems[x]);
+  }
+}
+
+window.onload = resizeAllGridItems();
+window.addEventListener("resize", resizeAllGridItems);
+
 loadScript('./js/ripple.js')
