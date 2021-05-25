aa8de34373f8383103dd78eb19f5f858e6d4108a
unknown <rizal.arfiyan.23@gmail.com>
2021-05-25 22:20:13 +0700

    index page

--- src/js/ripple.js
+++ src/js/ripple.js
@@ -1,4 +1,4 @@
-const buttons = document.querySelectorAll('button');
+const buttons = document.querySelectorAll('.btn');
 buttons.forEach(button => {
   button.onmousedown = function(e) {
     const $this = this
