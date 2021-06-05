e41883a808667292513c190a397b8e5f3e277693
unknown <rizal.arfiyan.23@gmail.com>
2021-06-05 08:02:11 +0700

    fix bugs height

--- src/js/scripts.js
+++ src/js/scripts.js
@@ -1,5 +1,5 @@
 import Ripple from './ripple.js'
 
 new Ripple({
-  element: '.btn, .ripple, .posts .card .card-image, .fast-access .card'
+  element: '.btn, .ripple, .posts .card .card-image, .fast-access .card, .extra-image'
 }).init()
